660ff8273a7001405e91a098c37c00dd268517f4
MaryLakhanich <amigo-10291@mail.ru>
2022-07-03 18:41:05 +0300

    latest  test version

--- src/test/java/SortableDataTables.java
+++ src/test/java/SortableDataTables.java
@@ -19,14 +19,43 @@ public class SortableDataTables extends BaseTest{
     }
     @Test
     public void sortTables () {
-        WebElement lastName=driver.findElement(By.xpath("//tbody/tr[1]/td[1]"));
-        Assert.assertEquals(lastName.getText(), "Smith");
-        WebElement firstName=driver.findElement(By.xpath("//tbody/tr[2]/td[2]"));
-        Assert.assertEquals(firstName.getText(), "Frank");
-        WebElement email=driver.findElement(By.xpath("//tbody/tr[2]/td[3]"));
-        Assert.assertEquals(email.getText(), "fbach@yahoo.com");
-        WebElement webSite=driver.findElement(By.xpath("//tbody/tr[3]/td[5]"));
-        Assert.assertEquals(webSite.getText(), "http://www.jdoe.com");
-
+        List<WebElement> elements = driver.findElements(By.cssSelector(".tablesorter thead tr th"));
+        List<WebElement> johnSmith = driver.findElements(By.xpath("//tbody/tr[1]/td"));
+        for (WebElement element : johnSmith) {
+            Assert.assertEquals(johnSmith.get(0).getText(), "Smith");
+            Assert.assertEquals(johnSmith.get(1).getText(), "John");
+            Assert.assertEquals(johnSmith.get(2).getText(), "jsmith@gmail.com");
+            Assert.assertEquals(johnSmith.get(3).getText(), "$50.00");
+            Assert.assertEquals(johnSmith.get(4).getText(), "http://www.jsmith.com");
+            Assert.assertEquals(johnSmith.get(5).getText(), "edit delete");
+        }
+        List<WebElement> bachFrank = driver.findElements(By.xpath("//tbody/tr[2]/td"));
+        for (WebElement element : bachFrank) {
+            Assert.assertEquals(bachFrank.get(0).getText(), "Bach");
+            Assert.assertEquals(bachFrank.get(1).getText(), "Frank");
+            Assert.assertEquals(bachFrank.get(2).getText(), "fbach@yahoo.com");
+            Assert.assertEquals(bachFrank.get(3).getText(), "$51.00");
+            Assert.assertEquals(bachFrank.get(4).getText(), "http://www.frank.com");
+            Assert.assertEquals(bachFrank.get(5).getText(), "edit delete");
+        }
+        List<WebElement> doeJason = driver.findElements(By.xpath("//tbody/tr[3]/td"));
+        for (WebElement element : doeJason) {
+            Assert.assertEquals(doeJason.get(0).getText(), "Doe");
+            Assert.assertEquals(doeJason.get(1).getText(), "Jason");
+            Assert.assertEquals(doeJason.get(2).getText(), "jdoe@hotmail.com");
+            Assert.assertEquals(doeJason.get(3).getText(), "$100.00");
+            Assert.assertEquals(doeJason.get(4).getText(), "http://www.jdoe.com");
+            Assert.assertEquals(doeJason.get(5).getText(), "edit delete");
+        }
+        List<WebElement> timConway = driver.findElements(By.xpath("//tbody/tr[4]/td"));
+        for (WebElement element : timConway) {
+            Assert.assertEquals(timConway.get(0).getText(), "Conway");
+            Assert.assertEquals(timConway.get(1).getText(), "Tim");
+            Assert.assertEquals(timConway.get(2).getText(), "tconway@earthlink.net");
+            Assert.assertEquals(timConway.get(3).getText(), "$50.00");
+            Assert.assertEquals(timConway.get(4).getText(), "http://www.timconway.com");
+            Assert.assertEquals(timConway.get(5).getText(), "edit delete");
+        }
     }
 }
+
